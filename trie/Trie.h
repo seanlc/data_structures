059ad4e85f53aa1960e2bf030efd9829d7dd3073
sean <seanlcarstensen@gmail.com>
2018-09-29 17:22:36 -0700

    added getPos() implementation

--- trie/Trie.h
+++ trie/Trie.h
@@ -1,4 +1,5 @@
 #include <string>
+#include <stdint.h>
 
 #define ALPHALEN 255
 #define NUMPOS 20
@@ -8,34 +9,63 @@ using namespace std;
 struct Node
 {
     char c;
-    int pos[20];
+    int pos[NUMPOS];
     int curPos;
     Node * branch[ALPHALEN];
-    Node()
+    int numBranches;
+
+    Node(char ch)
     {
+	c = ch;
         curPos = 0;
+	numBranches = 0;
 	for(int i = 0; i < ALPHALEN; ++i)
 	    branch[i] = nullptr;
     }
-    void getBranchPos(string s);
+
+    int getBranchPos(string s, int * pos, int * numPos)
+    {
+	uint8_t c = s[0];
+	printf("entering node %c looking for %c\n", this->c, c);
+        if(s.length() != 0)
+	{
+	  if(branch[c] == nullptr)
+	    *numPos = 0;
+	  else
+	    branch[c]->getBranchPos(s.substr(1), pos, numPos); 
+	}
+	else
+	{
+          *numPos = curPos;
+          for(int i = 0; i < curPos; ++i)
+	    pos[i] = this->pos[i];	  
+	}
+
+	return 0;
+    }
+
     int addBranch(string s, int pos)
     {
       if (s.length() != 0)
       {
-        int c = s[0];
+        uint8_t c = s[0];
 	if (branch[c] == nullptr)
-	  branch[c] = new Node();
-
+	{
+	  branch[c] = new Node(c);
+	  numBranches++;
+	}
         branch[c]->addBranch(s.substr(1), pos);	  
       }
       else
       {
         if(curPos < NUMPOS)
-	  this->pos[curPos] = pos;	
+	  this->pos[curPos++] = pos;	
       } 
       
       return 0;
     }
+
+    // find child of last node with >= 2 branches off and delete
     int removeBranch(char c);
 };
 
@@ -44,7 +74,7 @@ class Trie
   public:
     Trie()
     {
-        root = new Node();
+        root = new Node(0);
     }
     ~Trie()
     {
@@ -56,8 +86,13 @@ class Trie
        return 0;
     }
     int remove(string s);
-    int getPos(string s);
-    int print();
+    int getPos(string s, int * pos, int *numPos)
+    {
+       root->getBranchPos(s,pos,numPos); 
+       return 0;
+    }
+    int findLongestSubstring(string s, string substr, int pos);
+    int addCharToAll(char c);
   
   private:
     Node * root;
--- trie/main.cpp
+++ trie/main.cpp
@@ -11,13 +11,27 @@ void testConstrDestr()
 void testInsert()
 {
   Trie myT;
-//  myT.insert("test", 0);
-//  myT.insert("tets", 0);
+  myT.insert("test", 0);
+  myT.insert("tets", 0);
+}
+
+void testGet()
+{
+  int posArr[20];
+  int numPos = -1;
+  Trie myT;
+  myT.insert("string",7);
+  myT.insert("sean",6);
+  myT.getPos("strings",posArr, &numPos);
+  printf("numPos is %d\n", numPos);
+  for(int i = 0; i < numPos; ++i)
+    printf("pos: %d\n", posArr[i]);
 }
 
 int main()
 {
 //  testConstrDestr();
-  testInsert();
+//  testInsert();
+  testGet();
   return 0;
 }
